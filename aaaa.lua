1f7e1d3539b5e8186f8e2fd8390507436f490774
Chebasikas <nikita.nikita.novikov.novikov@gmail.com>
2025-04-03 12:37:39 +0300

    Add files via upload

--- aaaa.lua
+++ aaaa.lua
@@ -13,7 +13,8 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 11local script_vers_text = "5.05"
+local script_vers = 13
+local script_vers_text = "5.05"
 
 local update_url = "https://raw.githubusercontent.com/Chebasikas/scrit/refs/heads/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
